0e7ca55e12df8633ddad2b9ec95d49ac9db1fd8e
SDHINSA <sohanad@icloud.com>
2024-08-14 00:09:40 +0700

    Styling the starting point

--- src/index.js
+++ src/index.js
@@ -47,6 +47,11 @@ function renderBoards(player, show = true){
     })
 }
 
+function displayTurn(player){
+    const display = document.querySelector(".displayTurns");
+    display.innerHTML=`${player.name}'s turn`;
+}
+
 const startButton = document.getElementById("start");
 startButton.addEventListener("click", ()=>{
     const pOneName = document.getElementById("player1").value;
@@ -59,11 +64,12 @@ startButton.addEventListener("click", ()=>{
 
     renderBoards(playerOne);
     renderBoards(computer);
+    displayTurn(playerOne);
 
     
 })
 // Step 1. When press start button, it should render the boards and display whose turn 
-// things needed: render board, event listener on start button, create new instances of people, create the gameboards and place ships properly/randomly
+// things needed: render board - CHECK , event listener on start button - CHECK, create new instances of people -CHECK , create the gameboards and place ships properly/randomly
 
 //Step 2. After step 1 is done, put in logic for clicking on cell, this is when you add function to buttons that click the cell and see if hit
 // things needed: render board after each turn, function for clicking cell, making sure the same cell wasnt picked twice, making sure that its on the board, making sure that the game functions are called properly, handling how the computer takes turns, showing the items/misplaced, computer AI for taking turns
